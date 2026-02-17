aae8f4787a81c49a02a0668ac847f115864a10cf
David Chessington <davidadavaz@outlook.com>
2026-02-16 19:59:39 -0600

    Add new customer form and creation route

--- customer_api.py
+++ customer_api.py
@@ -7,6 +7,61 @@ import requests
 
 app=flask.Flask(__name__)
 
+
+dotenv.load_dotenv()
+if dotenv.dotenv_values("DATA_BASE")=="LOCAL":
+    import local_DB as DB
+else:
+    print("Cloud Database")
+
+url="http://127.0.0.1:5000"
+
+
+
+@app.route('/')
+def main():
+    return flask.render_template("index.html")
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+@app.route("/home")
+def home():
+    return flask.render_template("home.html")
+
+@app.route("/new",methods=['GET', 'POST'])
+def new_ui():
+    if flask.request.method=="POST":
+        print(flask.request)
+        data=flask.request.form
+        try:
+            user_exists = DB.searchCustomer(data['username'])
+            if user_exists:
+                flask.flash(f"El usuario '{data['username']}' ya está registrado.", "danger")
+                return flask.redirect("/new")
+            DB.newCustomer(Customer(**data))
+            message="customer succesfully added"
+        except Exception as ex:
+            message=f"An error ocured:{ex}"
+        return flask.render_template("new.html",response=message)
+
+    return flask.render_template("new.html")
+
+@app.route("/search")
+def search_ui():
+    return flask.render_template("customer.html")
+
+@app.route("/list")
+def list_ui():
+    response=requests.request(method="GET",url=f"{url}/customers").json()
+    return flask.render_template("customers.html",context=response)
+
+@app.route("/delete")
+def delte_ui():
+    return flask.render_template("delete.html")
+
+
 @app.route('/customers')
 def list():
     try:
